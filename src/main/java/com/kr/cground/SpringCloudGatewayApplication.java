f52eaf5c1203e52e7d689d6123e77f17e5fa00dd
beomshin.kim <chzh859@naver.com>
2024-10-02 19:37:48 +0900

    # gateway 기본 설정

--- src/main/java/com/kr/cground/SpringCloudGatewayApplication.java
+++ src/main/java/com/kr/cground/SpringCloudGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.springcloudgateway;
+package com.kr.cground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/kr/cground/SpringCloudGatewayApplicationTests.java
+++ src/test/java/com/kr/cground/SpringCloudGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springcloudgateway;
+package com.kr.cground;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
